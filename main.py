b89f7efea7f12cc49a152e8fbf35e8057d20d3c5
UNILESS <44114950+UNILESS@users.noreply.github.com>
2021-03-17 14:29:35 +0900

    3/17 BS4 + request for wikipedia

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ headers = {
     "Accept": "text_html,application_xhtml+xml,application_xml;q=0.9,image_webp,**/**;q=0.8",
     "Connection": "close"
 }
+Url = "https://ko.wikipedia.org/wiki/"
+
+title = input("검색할 단어를 입력해주세요. ")
+
+Url_title = Url + title
+
+soup = BeautifulSoup(session.get(Url_title, headers=headers).content, "html.parser")
