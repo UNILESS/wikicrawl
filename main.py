bffe3ae4d830b8056ffa72de61ba7fee74e6ee62
UNILESS <44114950+UNILESS@users.noreply.github.com>
2021-03-19 13:16:20 +0900

    3/17 BS4 + request for wikipedia

--- main.py
+++ main.py
@@ -4,7 +4,39 @@ from urllib.request import urlopen
 from urllib.request import HTTPError
 from bs4 import BeautifulSoup
 
-session = requests.Session()
+
+class Content:
+    def __init__(self, url, title, body):
+        self.url = url
+        self.title = title
+        self.body = body
+
+
+def getPage(url):
+    req = requests.get(url)
+    return BeautifulSoup(req.text, 'html.parser')
+
+
+def scrapeWiki(url):
+    bs = getPage(url)
+    title = bs.find('h1').text
+    lines = bs.select('p')
+    body = '\n'.join([line.text for line in lines])
+    return Content(url, title, body)
+
+
+url = "https://ko.wikipedia.org/wiki/"
+
+word = urllib.parse.quote(input("검색할 단어를 입력해주세요. \n"))
+Url_word = (url + str(word).replace("'", ""))
+
+content = scrapeWiki(Url_word)
+print('Title: {}'.format(content.title))
+print('URL: {}'.format(content, Url_word))
+print(content.body)
+
+
+'''session = requests.Session()
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit 537.36 (KHTML, like Gecko) Chrome",
     "Accept": "text_html,application_xhtml+xml,application_xml;q=0.9,image_webp,**/**;q=0.8",
@@ -37,3 +69,4 @@ if title is None:
 else:
     for docs in soup:
         print(docs.get_text())
+'''
